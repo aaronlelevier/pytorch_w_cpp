09976ab6406d2447bd34c682b43475c8212a80ab
aaronlelevier <aaron.lelevier@gmail.com>
2019-03-02 08:09:26 -0800

    working example of me first loading a PyTorch 'torch.jit.ScriptModule' to C++ and then successfully calling it from the compiled C++ file

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+I could follow the tutorial [LOADING A PYTORCH MODEL IN C++](https://pytorch.org/tutorials/advanced/cpp_export.html) and get the PyTorch Model converted and callable to C++ correctly
--- example-app.cpp
+++ example-app.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 #include <memory>
 
-int main(int argc, const char* argv[]) {
-  if (argc != 2) {
+int main(int argc, const char *argv[])
+{
+  if (argc != 2)
+  {
     std::cerr << "usage: example-app <path-to-exported-script-module>\n";
     return -1;
   }
@@ -14,4 +16,15 @@ int main(int argc, const char* argv[]) {
 
   assert(module != nullptr);
   std::cout << "ok\n";
+
+  // Create a vector of inputs.
+  std::vector<torch::jit::IValue> inputs;
+  inputs.push_back(torch::ones({1, 3, 224, 224}));
+
+  // Execute the model and turn its output into a tensor.
+  at::Tensor output = module->forward(inputs).toTensor();
+
+  std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
+
+  return 0;
 }
